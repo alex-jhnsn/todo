a624fe965ab378095278ef377a732b9fd612eb73
Alex Johnson <alex.johnson@myunidays.com>
2022-03-22 16:35:27 +0000

    Add basics of flask server

--- client/index.js
+++ client/index.js
@@ -1,29 +1,56 @@
-function getData () {
+const data = [
+  {
+    "id": 1,
+    "isComplete": false,
+    "value": "Make to do website"
+  },
+  {
+    "id": 2,
+    "isComplete": false,
+    "value": "Release meaa-4"
+  },
+  {
+    "id": 3,
+    "isComplete": false,
+    "value": "Complete qa"
+  },
+  {
+    "id": 4,
+    "isComplete": true,
+    "value": "Go get a coffee"
+  },
+];
+
+function getLocalData () {
   return new Promise(resolve => {
     resolve(data);
   });
 }
 
-const data = [
-  {
-    "complete": false,
-    "value": "Make to do website"
-  },
-  {
-    "complete": false,
-    "value": "Release meaa-4"
-  },
-  {
-    "complete": false,
-    "value": "Complete qa"
-  },
-  {
-    "complete": false,
-    "value": "Go get a coffee"
-  },
-];
+function getData () {
+  return fetch('http://127.0.0.1:5000/todo')
+    .then(res => res.json())
+    .then(data => {
+      return data;
+    });
+}
 
-getData()
+function renderItem (id, isComplete, value) {
+  return `<fieldset class="list-item" data-id="${id}" >
+    <input type="checkbox" ${isComplete ? "checked" : ""} />
+    <input type="text" value="${value}" />
+  </fieldset>`
+}
+
+window.onload = () => {
+  const $todoContainer = document.querySelector('.todo-container');
+
+  getData()
   .then(data => {
-    console.log(data);
+    var content = '';
+    data.forEach(item => {
+      content += renderItem(item.id, item.isComplete, item.value);
+    });
+    $todoContainer.innerHTML = content
   });
+};
---
+++ server/api.py
@@ -0,0 +1,15 @@
+import json
+from flask import Flask
+#from flask_restful import Resource, Api
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/todo")
+def helloWorld():
+    with open("data.json", "r") as json_file:
+        data = json_file.read()
+        return data
+
+app.run()
