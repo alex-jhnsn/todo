acf4796545080323bccb3c15963f23d5a7447e71
Alex Johnson <alextheviking@hotmail.co.uk>
2022-03-28 09:34:26 +0100

    Change gitignore

--- python_server/data.py
+++
@@ -1,8 +0,0 @@
-def getData():
-    with open("data.json", "r") as json_file:
-        data = json_file.read()
-        return data
-
-def saveData(data):
-    with open("data.json", "w") as json_file:
-        data = json.dump(data, json_file)
